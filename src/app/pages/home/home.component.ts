bf8c240372d39a993dafb5f8810b26988ad21e36
Daniel Cruz <ldanicp1998@gmail.com>
2024-11-05 11:17:49 +0100

    First commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, signal, WritableSignal } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +12,54 @@ import { ApiService } from '../../services/api.service';
 })
 export class HomeComponent {
   api = inject(ApiService);
+  router = inject(Router);
 
-  ngOnInit() {
-    let request = this.api.getNationalities();
-    request.subscribe(
-      (data:any)=>{
-        console.log(data);
-      }
+  state:WritableSignal<any> = signal({
+    type:'nationality',
+    loading:false,
+    error:false,
+    data:[]
+  });
+
+  @Input()
+  set type(type: string) {
+    //1 - Analizar el valor,
+    //2 - Llamar al servicio
+
+    // Hemos recibido un cambio en la ruta
+    this.state.update(state => (
+      {...state, loading:true, type:type}
+      )
     );
+
+    let request:Observable<any>;
+    switch (type) {
+      case 'category':
+        request = this.api.getCategories()
+        break;
+      default:
+        request = this.api.getNationalities()
+        break;
+    }
+    request.subscribe((data:any) => {
+      this.state.update(state => (
+        {...state, loading:false, error:false, 
+          data:data.map((m:any) => (
+            type == 'category' ? ({name:m.strCategory}) : ({name:m.strArea})))
+        }
+      ))
+    console.log(data)
+    },
+    (err) => {
+      console.log(err)
+      this.state.update(state => (
+        {...state, loading:false, error:true, data:[]}
+      ))
+    });
+  }
+
+  listRecipes(ingredient: string) {
+    // Ir a la página /recipe/tipo/ingredient
+    this.router.navigate(['recipes', this.state().type, ingredient]);
   }
 }
