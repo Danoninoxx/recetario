e9d396949488c9fcfcc371c27e96bf29dc200412
Daniel Cruz <ldanicp1998@gmail.com>
2024-11-08 09:34:18 +0100

    Añadido el menu bar medio en condiciones

---
+++ src/app/model/nationality.ts
@@ -0,0 +1,3 @@
+export interface Nationality {
+    strArea: string;
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GoogleAuthProvider } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  router = inject(Router);
+  userData: any = null;
+  $isLoggedIn: WritableSignal<boolean> = signal(false);
+
+  constructor(private auth: AngularFireAuth) {
+    //Comprobar si el usuario esta logeado automaticamente
+    this.auth.authState.subscribe(data=>{
+      if(data){
+        //Estoy logeado
+        this.userData = data;
+        this.$isLoggedIn.set(true);
+      }else{
+        //No estoy logeado
+        this.userData = null;
+        this.$isLoggedIn.set(false);
+        this.router.navigate(['login']);
+      }
+    })
+  }
+
+  async login(){
+    try{
+      let user = await this.auth.signInWithPopup(new GoogleAuthProvider());
+      return user;
+    }catch(err){
+      return err;
+    }
+    
+  }
+
+  logout(){
+    this.auth.signOut();
+  }
 }
